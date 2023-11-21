7a8f7d5f38fff91a3c1dd7911235fcdfc8c75c18
rum <mustbagus1337@gmail.com>
2023-11-21 20:57:51 +0700

    Modify database settings

--- backend/settings.py
+++ backend/settings.py
@@ -130,8 +130,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'open-netra',
+        'USER': 'postgres',
+        'PASSWORD': 'opennetra',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
